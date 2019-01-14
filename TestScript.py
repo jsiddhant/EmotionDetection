269cd54f9f06747b7032f30c19f800ce36898e86
Siddhant Jain <sidjain189@gmail.com>
2019-01-14 15:04:33 -0800

    Generalized the training procedure and added functionality for Logistic Regression

--- TestScript.py
+++ TestScript.py
@@ -59,3 +59,83 @@ I8 = (((I - I.min()) / (I.max() - I.min())) * 255.9).astype(np.uint8)
 DL.display_face(I8)
 
 print(p.test_PCA(eigFace))
+
+# # Prep Training Data
+#
+# trainTotal = list(set(cafe.trainHappy + cafe.trainSad))
+# trainImages = []  # Total Training Set
+# trainLabels = []  # Total Training Image Set
+# for t in trainTotal:
+#     trainImages.append(t.image)
+#     trainLabels.append(1.0 if t.emotion == 'h' else 0.0)
+#
+# trainImages = np.asarray(trainImages)
+# trainLabels = np.asarray(trainLabels)
+#
+# # Prep HoldOut Data
+#
+# holdTotal = list(set(cafe.holdHappy + cafe.holdSad))
+# holdImages = []
+# holdLabels = []
+# for t in holdTotal:
+#     holdImages.append(t.image)
+#     holdLabels.append(1.0 if t.emotion == 'h' else 0.0)
+#
+# holdImages = np.asarray(holdImages)
+# holdLabels = np.asarray(holdLabels)
+#
+# # Prep Test Data
+#
+# testTotal = list(set(cafe.testHappy + cafe.testSad))
+# testImages = []
+# testLabels = []
+# for t in testTotal:
+#     testImages.append(t.image)
+#     testLabels.append(1.0 if t.emotion == 'h' else 0.0)
+#
+# testImages = np.asarray(testImages)
+# testLabels = np.asarray(testLabels)
+
+# # PCA
+# n_components = 16
+# eigComps, _, _ = PCA.PCA(trainImages, n_components)
+
+# # Uncomment to show EigenFaces
+# # eigFace = eigComps.reshape(n_components, height, width)
+# # for i in range(0, 16):
+# #     visualize_image(eigFace[i])
+#
+# #########################################################
+# #########################################################
+#
+# #########################################################
+# # PA:1 -- Q.2
+# #########################################################
+# parameter_num = 10
+# eigComps = eigComps[0:parameter_num]
+
+# # trainImageVec = trainImages.reshape(len(trainTotal), height*width)
+# trainImageVec = trainImages.reshape(len(trainTotal), height*width)
+# # trainImageVec = (trainImageVec.T / np.linalg.norm(trainImageVec, 2, axis=1)).T
+# trainImageVec = norm_vec(trainImageVec)
+#
+# holdImageVec = holdImages.reshape(len(holdTotal), height * width)
+# holdImageVec = norm_vec(holdImageVec)
+
+# x_proj = np.dot(eigComps, trainImageVec.T)
+# x_h_proj = np.dot(eigComps, holdImageVec.T)
+# w = np.random.rand(parameter_num)
+# alpha = 0.1  # 0.01
+#
+# for epoch in range(0, 10):
+#     y = eval_logistic(w, x_proj)
+#     yH = eval_logistic(w, x_h_proj)
+#     cross_entropy_loss(trainLabels, y)
+#     print("Epoch: " + str(epoch) + " SSE Training Error: " + str(eval_error(trainLabels, y)) + " SSE Val Error: " + str(eval_error(holdLabels, yH)))
+#     print("Epoch: " + str(epoch) + " Training Error: " + str(cross_entropy_loss(trainLabels, y)) + " Val Error: " + str(cross_entropy_loss(holdLabels, yH)))
+#     w = update_w_logistic(trainLabels, y, x_proj, alpha, w)
+# y = eval_logistic(w, x_proj)
+# yH = eval_logistic(w, x_h_proj)
+# print(eigComps.shape)
+#
+# ## Update Loss function to CrossEntropy
--- emotionScripts.py
+++ emotionScripts.py
@@ -13,8 +13,10 @@ import PCA
 
 class DataSet:
 
-    def __init__(self, data, data_labels):
+    def __init__(self, data, data_labels, height=380, width=240, testPeople= None, holdPeople = None, emotionList=['h','m']):
 
+        self.height = height
+        self.width = width
         # Create a Map from Label to Data
         dataMap={}
         for i in range(0,len(data_labels)):
@@ -33,11 +35,26 @@ class DataSet:
         self.emotions = list(emotionCount.keys())
         self.people = list(personCount.keys())
 
-        # Get No. of Images by Subject and Emotion
-        self.get_data_stats()
+        # Create a set of training data by first selecting people in the 80:10:10 ratio.
+        # TestPeople / HoldPeople can be used optionally specifying people to keep for respective set
+        # Otherwise one is chosen randomly.
 
-        # Generate Train, Hold and Test Set for h and m emotions
-        self.data_split()
+        people_list_train = self.people
+
+        testPeople = random.choice(people_list_train) if testPeople is None else testPeople
+        people_list_train.remove(testPeople)
+        holdPeople = random.choice(people_list_train) if holdPeople is None else holdPeople
+        people_list_train.remove(holdPeople)
+
+        self.trainSet = self.get_filtered(people_list_train, emotionList)
+        self.testSet = self.get_filtered(testPeople, emotionList)
+        self.holdSet = self.get_filtered(holdPeople, emotionList)
+
+        # # Get No. of Images by Subject and Emotion
+        # self.get_data_stats()
+        #
+        # # Generate Train, Hold and Test Set for h and m emotions
+        # self.data_split()
 
     def get_data_stats(self):
         # Get Number of Images for each emotion and each person.
@@ -69,35 +86,52 @@ class DataSet:
     def get_filtered(self, person, emotion):
         return [data for data in self.dataList if data.subject in person and data.emotion in emotion]
 
-    def data_split(self):
-
-        people_list = self.people
-        random.shuffle(people_list)
-        print('------------------')
-
-        self.trainHappy = self.get_filtered(people_list[:8], ['h'])
-        print("Length Train Happy: ")
-        print(len(self.trainHappy))
-
-        self.trainSad = self.get_filtered(people_list[:8], ['m'])
-        print("Length Train Sad: ")
-        print(len(self.trainSad))
-
-        self.holdHappy = self.get_filtered(people_list[8], ['h'])
-        print("Length Hold Happy: ")
-        print(len(self.holdHappy))
-
-        self.holdSad = self.get_filtered(people_list[8], ['m'])
-        print("Length Hold Sad: ")
-        print(len(self.holdSad))
-
-        self.testHappy = self.get_filtered(people_list[9], ['h'])
-        print("Length Test Happy: ")
-        print(len(self.testHappy))
-
-        self.testSad = self.get_filtered(people_list[9], ['m'])
-        print("Length Test Sad: ")
-        print(len(self.testSad))
+    # def createSets(self, testPeople= None, holdPeople = None, emotionList=['h','m']):
+    #
+    #     # Create a set of training data by first selecting people in the 80:10:10 ratio.
+    #     # TestPeople / HoldPeople can be used optionally specifying people to keep for respective set
+    #     # Otherwise one is chosen randomly.
+    #
+    #     people_list_train = self.people()
+    #
+    #     testPeople = random.choice(people_list_train) if testPeople is None else testPeople
+    #     people_list_train.remove(testPeople)
+    #     holdPeople = random.choice(people_list_train) if holdPeople is None else holdPeople
+    #     people_list_train.remove(holdPeople)
+    #
+    #     self.trainSet = self.get_filtered(people_list_train, emotionList)
+    #     self.testSet = self.get_filtered(testPeople, emotionList)
+    #     self.holdSet = self.get_filtered(holdPeople, emotionList)
+    #
+    # def data_split(self, testPeople = None, testEmotions = ['h','m']):
+    #
+    #     people_list = self.people
+    #     random.shuffle(people_list)
+    #     print('------------------')
+    #
+    #     self.trainHappy = self.get_filtered(people_list[:8], ['h'])
+    #     print("Length Train Happy: ")
+    #     print(len(self.trainHappy))
+    #
+    #     self.trainSad = self.get_filtered(people_list[:8], ['m'])
+    #     print("Length Train Sad: ")
+    #     print(len(self.trainSad))
+    #
+    #     self.holdHappy = self.get_filtered(people_list[8], ['h'])
+    #     print("Length Hold Happy: ")
+    #     print(len(self.holdHappy))
+    #
+    #     self.holdSad = self.get_filtered(people_list[8], ['m'])
+    #     print("Length Hold Sad: ")
+    #     print(len(self.holdSad))
+    #
+    #     self.testHappy = self.get_filtered(people_list[9], ['h'])
+    #     print("Length Test Happy: ")
+    #     print(len(self.testHappy))
+    #
+    #     self.testSad = self.get_filtered(people_list[9], ['m'])
+    #     print("Length Test Sad: ")
+    #     print(len(self.testSad))
 
 
 class DataItem:
@@ -114,15 +148,6 @@ def visualize_image(image):
     DL.display_face(I_uint8)
 
 
-def eval_logistic(w,x):
-    return 1 / (1 + np.exp(-np.dot(w, x)))
-
-
-def update_w_logistic(t, y, x, al, w):
-    update = al * np.dot(t-y, x.T)
-    return w + update
-
-
 def eval_error(t, y):
     return np.dot(t-y, t-y)
 
@@ -131,6 +156,143 @@ def norm_vec(img):
     img = img.T - img.mean(axis=1)
     img = (img.T / np.linalg.norm(img, 2, axis=1))
     return img
+
+
+def cross_entropy_loss(label,prediction, eps = 1e-12):
+    prediction = np.clip(prediction, eps, 1. - eps)
+    n = prediction.shape[0]
+    ce = -np.sum(label*np.log(prediction+1e-9))/n
+    return ce
+
+
+class TrainInstance:
+
+    def __init__(self, dataset, parameter_num= 10):
+
+        self.parameter_num = parameter_num
+        self.trainError = np.Inf
+        self.holdError = np.Inf
+        self.testError = np.Inf
+
+        h = dataset.height
+        w = dataset.width
+
+        # Prep Training Data
+        trainTotal = list(set(dataset.trainSet))
+        trainImages = []  # Total Training Set
+        trainLabels = []  # Total Training Image Set
+
+        for t in trainTotal:
+            trainImages.append(t.image)
+            trainLabels.append(1.0 if t.emotion == 'h' else 0.0)
+
+        trainImages = np.asarray(trainImages)
+        self.trainLabels = np.asarray(trainLabels)
+
+        # Prep HoldOut Data
+
+        holdTotal = list(set(dataset.holdSet))
+        holdImages = []
+        holdLabels = []
+        for t in holdTotal:
+            holdImages.append(t.image)
+            holdLabels.append(1.0 if t.emotion == 'h' else 0.0)
+
+        holdImages = np.asarray(holdImages)
+        self.holdLabels = np.asarray(holdLabels)
+
+        # Prep Test Data
+
+        testTotal = list(set(dataset.testSet))
+        testImages = []
+        testLabels = []
+        for t in testTotal:
+            testImages.append(t.image)
+            testLabels.append(1.0 if t.emotion == 'h' else 0.0)
+
+        testImages = np.asarray(testImages)
+        self.testLabels = np.asarray(testLabels)
+
+        eigComps, _, _ = PCA.PCA(trainImages, parameter_num)
+        self.eigComps = eigComps[0:parameter_num]
+
+        trainImageVec = trainImages.reshape(len(trainTotal), h * w)
+        self.trainImageVec = norm_vec(trainImageVec)
+
+        holdImageVec = holdImages.reshape(len(holdTotal), h * w)
+        self.holdImageVec = norm_vec(holdImageVec)
+
+        testImageVec = testImages.reshape(len(testTotal), h * w)
+        self.testImageVec = norm_vec(testImageVec)
+
+        self.inputTrain = np.dot(self.eigComps, self.trainImageVec.T)
+        self.inputHold = np.dot(self.eigComps, self.holdImageVec.T)
+        self.inputTest = np.dot(self.eigComps, self.testImageVec.T)
+
+    def batch_gradient_descent(self, model, epochs):
+
+        x_proj = self.inputTrain
+        x_h_proj = self.inputHold
+
+        for epoch in range(0, epochs):
+
+            y = model.eval(x_proj)
+            yH = model.eval(x_h_proj)
+            cross_entropy_loss(self.trainLabels, y)
+
+            self.trainError = cross_entropy_loss(self.trainLabels, y)
+            self.holdError = cross_entropy_loss(self.holdLabels, yH)
+            print("=========================================================================================")
+            print("Epoch: " + str(epoch) + " SSE Training Error: " + str(
+                eval_error(self.trainLabels, y)) + " SSE Val Error: " + str(eval_error(self.holdLabels, yH)))
+
+            print("Epoch: " + str(epoch) + " Training Error: " + str(
+                self.trainError) + " Val Error: " + str(self.holdError))
+            print("=========================================================================================")
+            model.early_stopping(cross_entropy_loss(self.holdLabels, yH))
+            model.update_w(self.trainLabels, y, x_proj)
+
+            # Implement Graphing of Training and Hold Out Error
+
+    def get_test_error(self, model):
+        # count = 0
+        # correct = 0
+        # for i in range(0, len(self.holdLabels)):
+        #     count = count+1
+        #     label_test = 1 if model.eval(self.inputHold[:, i]) > 0.5 else 0
+        #     correct = correct +1 if self.holdLabels[i] == label_test else correct
+        #
+        # self.testError = 1- np.float(correct/count)
+
+        count = 0
+        correct = 0
+        for i in range(0, len(self.testLabels)):
+            count = count+1
+            label_test = 1 if model.eval(self.inputTest[:, i]) > 0.5 else 0
+            correct = correct +1 if self.testLabels[i] == label_test else correct
+
+        self.testError = 1 - np.float(correct/count)
+
+class LogisticModel:
+
+    def __init__(self, learningRate, parameter_num):
+        self.w = np.random.rand(parameter_num)*0.1
+        self.w_final = self.w
+        self.alpha = learningRate
+        self.minHoldError = np.Inf
+
+    def eval(self, input):
+        return 1 / (1 + np.exp(-np.dot(self.w, input)))
+
+    def update_w(self, target, y, x):
+        update = self.alpha * np.dot(target - y, x.T)
+        self.w = self.w + update
+
+    def early_stopping(self, hold_error):
+
+        self.minHoldError = hold_error if hold_error < self.minHoldError else self.minHoldError
+        self.w_final = self.w if hold_error < self.minHoldError else self.w_final
+
 
 
 if __name__ == "__main__":
@@ -147,77 +309,14 @@ if __name__ == "__main__":
     numImgs, height, width = data_img.shape
     cafe = DataSet(data_img, labels)
 
-    # Prep Training Data
+    param_num = 10
+    logisticTrain = TrainInstance(cafe, param_num)
+    logisticRegression = LogisticModel(0.1, param_num)
+    logisticTrain.batch_gradient_descent(logisticRegression, 10)
 
-    trainTotal = list(set(cafe.trainHappy + cafe.trainSad))
-    trainImages = []  # Total Training Set
-    trainLabels = []  # Total Training Image Set
-    for t in trainTotal:
-        trainImages.append(t.image)
-        trainLabels.append(1.0 if t.emotion == 'h' else 0.0)
+    logisticTrain.get_test_error(logisticRegression)
 
-    trainImages = np.asarray(trainImages)
-    trainLabels = np.asarray(trainLabels)
+    print("===================================================")
+    print("Test Error is: " + str(logisticTrain.testError))
+    print("===================================================")
 
-    # Prep HoldOut Data
-
-    holdTotal = list(set(cafe.holdHappy + cafe.holdSad))
-    holdImages = []
-    holdLabels = []
-    for t in holdTotal:
-        holdImages.append(t.image)
-        holdLabels.append(1.0 if t.emotion == 'h' else 0.0)
-
-    holdImages = np.asarray(holdImages)
-    holdLabels = np.asarray(holdLabels)
-
-    # Prep Test Data
-
-    testTotal = list(set(cafe.testHappy + cafe.testSad))
-    testImages = []
-    testLabels = []
-    for t in testTotal:
-        testImages.append(t.image)
-        testLabels.append(1.0 if t.emotion == 'h' else 0.0)
-
-    testImages = np.asarray(testImages)
-    testLabels = np.asarray(testLabels)
-
-    # PCA
-    n_components = 16
-    eigComps, _, _ = PCA.PCA(trainImages, n_components)
-
-    # Uncomment to show EigenFaces
-    # eigFace = eigComps.reshape(n_components, height, width)
-    # for i in range(0, 16):
-    #     visualize_image(eigFace[i])
-
-    #########################################################
-    #########################################################
-
-    #########################################################
-    # PA:1 -- Q.2
-    #########################################################
-    eigComps = eigComps[0:10]
-
-    # trainImageVec = trainImages.reshape(len(trainTotal), height*width)
-    trainImageVec = trainImages.reshape(len(trainTotal), height*width)
-    # trainImageVec = (trainImageVec.T / np.linalg.norm(trainImageVec, 2, axis=1)).T
-    trainImageVec = norm_vec(trainImageVec)
-
-    holdImageVec = holdImages.reshape(len(holdTotal), height * width)
-    holdImageVec = norm_vec(holdImageVec)
-
-    x_proj = np.dot(eigComps, trainImageVec.T)
-    x_h_proj = np.dot(eigComps, holdImageVec.T)
-    w = np.random.rand(10)*0.01
-    alpha = 0.01
-
-    for epoch in range(0, 10):
-        y = eval_logistic(w, x_proj)
-        yH = eval_logistic(w, x_h_proj)
-        print("Epoch: " + str(epoch) + " Training Error: " + str(eval_error(trainLabels, y)) + " Val Error: " + str(eval_error(holdLabels, yH)))
-        w = update_w_logistic(trainLabels, y, x_proj, alpha, w)
-    y = eval_logistic(w, x_proj)
-    yH = eval_logistic(w, x_h_proj)
-    print(eigComps.shape)
